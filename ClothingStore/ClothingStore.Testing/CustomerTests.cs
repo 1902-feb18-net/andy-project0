dff3804daa20fc101be95d81ab14aa7898068760
Andy Ly <andywindsong@gmail.com>
2019-03-05 10:01:05 -0600

    accidentally made context as a console app instead of a library

---
+++ ClothingStore/ClothingStore.Testing/CustomerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingStore.Testing
+{
+    public class CustomerTests
+    {
+    }
+}
---
+++ ClothingStore/ClothingStore.Testing/ProductTest.cs
@@ -0,0 +1,11 @@
+﻿using ClothingStore.Lib;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace ClothingStore.Testing
+{
+    public class ProductTest
+    {
+    }
+}
